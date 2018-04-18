67bc55d7188dbad78b2e17d961617cd544f196b5
logicalguess <logicalguess@gmail.com>
2018-04-17 22:21:40 -0400

    teleportation

--- test/src/test/java/logicalguess/test/simulator/TeleportationTest.java
+++ test/src/test/java/logicalguess/test/simulator/TeleportationTest.java
@@ -18,43 +18,42 @@ public class TeleportationTest {
         Circuit circuit = new Circuit(3);
         circuit.setStart();
 
-        // prepare state
-        //circuit.addGate(new X(new int[]{0}));
-
         // entangle pair
         circuit.addGate(new H(new int[]{1}));
         circuit.addGate(new CNOT(new int[]{1, 2}));
 
-        circuit.restOfSteps();
+        // prepare state
+        //circuit.addGate(new X(new int[]{0}));
 
 
         // encode
         circuit.addGate(new CNOT(new int[]{0, 1}));
         circuit.addGate(new H(new int[]{0}));
 
-        circuit.restOfSteps();
-
         circuit.measure(0);
         circuit.measure(1);
 
         circuit.restOfSteps();
-        int q0 = (int) Math.round(circuit.state.probQOne(0));
-        int q1 = (int) Math.round(circuit.state.probQOne(1));
 
-        LOG.info("measured state of qubit 0: " + q0);
-        LOG.info("measured state of qubit 1: " + q1);
+        int c0 = (int) Math.round(circuit.state.probQOne(0));
+        int c1 = (int) Math.round(circuit.state.probQOne(1));
+
+        LOG.info("measured state of qubit 0: " + c0);
+        LOG.info("measured state of qubit 1: " + c1);
 
         // decode
-        if (q0 == 1) circuit.addGate(new Z(new int[]{2}));
-        if (q1 == 1) circuit.addGate(new X(new int[]{2}));
+        if (c0 == 1) circuit.addGate(new Z(new int[]{2}));
+        if (c1 == 1) circuit.addGate(new X(new int[]{2}));
+        
+        LOG.info("probability of qubit 2 being 1: " + circuit.state.probQOne(2));
+
+        circuit.measure(2);
 
         circuit.restOfSteps();
 
-        LOG.info("probability of qubit 2 being 1: " + circuit.state.probQOne(2));
-        circuit.measure(2);
-        int q2 = (int) Math.round(circuit.state.probQOne(2));
+        int c2 = (int) Math.round(circuit.state.probQOne(2));
 
-        LOG.info("measured state of qubit 2: " + q2);
+        LOG.info("measured state of qubit 2: " + c2);
 
 
 
