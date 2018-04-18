56f22b03383893fa68600ccd5b68961c229e7d56
logicalguess <logicalguess@gmail.com>
2018-04-17 22:11:54 -0400

    teleportation

--- test/src/test/java/logicalguess/test/simulator/TeleportationTest.java
+++ test/src/test/java/logicalguess/test/simulator/TeleportationTest.java
@@ -18,25 +18,45 @@ public class TeleportationTest {
         Circuit circuit = new Circuit(3);
         circuit.setStart();
 
+        // prepare state
+        //circuit.addGate(new X(new int[]{0}));
+
+        // entangle pair
         circuit.addGate(new H(new int[]{1}));
         circuit.addGate(new CNOT(new int[]{1, 2}));
 
         circuit.restOfSteps();
 
-        circuit.addGate(new CNOT(new int[]{2, 1}));
-        circuit.addGate(new H(new int[]{2}));
+
+        // encode
+        circuit.addGate(new CNOT(new int[]{0, 1}));
+        circuit.addGate(new H(new int[]{0}));
 
         circuit.restOfSteps();
 
-        circuit.measure(2);
+        circuit.measure(0);
         circuit.measure(1);
 
         circuit.restOfSteps();
+        int q0 = (int) Math.round(circuit.state.probQOne(0));
+        int q1 = (int) Math.round(circuit.state.probQOne(1));
 
-        if ((circuit.state.probQOne(1)) > 0) circuit.addGate(new X(new int[]{0}));
-        if ((circuit.state.probQOne(2)) > 0) circuit.addGate(new Z(new int[]{0}));
+        LOG.info("measured state of qubit 0: " + q0);
+        LOG.info("measured state of qubit 1: " + q1);
+
+        // decode
+        if (q0 == 1) circuit.addGate(new Z(new int[]{2}));
+        if (q1 == 1) circuit.addGate(new X(new int[]{2}));
 
         circuit.restOfSteps();
+
+        LOG.info("probability of qubit 2 being 1: " + circuit.state.probQOne(2));
+        circuit.measure(2);
+        int q2 = (int) Math.round(circuit.state.probQOne(2));
+
+        LOG.info("measured state of qubit 2: " + q2);
+
+
 
     }
 }
